34328fe5493e6fe515426efb5799911a1bdce210
Yarvis54 <gfdtk66@yandex.ru>
2025-07-15 16:53:59 +0700

    12

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -23,3 +23,69 @@ else
 {
     WriteLine("o is not an int so it cannot multiply!");
 }
+
+WriteLine("\n\n\n");
+
+/*int number = (new Random()).Next(1, 7);
+WriteLine($"My random number id {number}");
+
+switch(number)
+{
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        goto case 1;
+    case 3:
+    case 4:
+        WriteLine("Three or four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+}
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");*/
+
+string path = @"D:\pae\progging\c-\Chapter03";
+Write("Press R for read-only or any other for writable: ");
+ConsoleKeyInfo  key = ReadKey();
+WriteLine();
+
+Stream? s;
+
+if(key.Key == ConsoleKey.R)
+{
+    s = File.Open(Path.Combine(path,"file.txt"),FileMode.OpenOrCreate,FileAccess.Read);
+}
+else
+{
+    s = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Write);
+}
+
+string message;
+
+switch(s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that i can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is read-only file.";
+        break;
+    case MemoryStream ms:
+        message = "The stream is a memory address.";
+        break;
+    default:
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+        break;
+}
+
+WriteLine(message);
