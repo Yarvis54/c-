8ab686cab7cf46b4187e4c03349416ad88beca8e
Yarvis54 <gfdtk66@yandex.ru>
2025-07-20 22:05:33 +0700

    5

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -2,12 +2,15 @@
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-test();
-
-///<summary>
-///dfdsf
-///</summary>
-static void test()
-{
-
+static double Add(double a, double b)
+{ 
+    return a + b; 
 }
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+
+WriteLine("Press ENTER to end the app.");
+ReadLine();
---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true);
+IConfigurationRoot configuration = builder.Build();
+
+
+
+Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log_test.txt"))));
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
