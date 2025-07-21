13c7127c4aa6b994130baea3485f42b2096fd7de
Yarvis54 <gfdtk66@yandex.ru>
2025-07-21 16:34:37 +0700

    6

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -4,14 +4,12 @@ using Microsoft.Extensions.Configuration;
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-ConfigurationBuilder builder = new();
-builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true);
-IConfigurationRoot configuration = builder.Build();
-
-
-
 Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log_test.txt"))));
 Trace.AutoFlush = true;
 
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine("Trace says, I am watching!");
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true);
+IConfigurationRoot configuration = builder.Build();
